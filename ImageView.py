27d5c9f81bda25cb766bcbaef5cf29e94d516e64
Mukesh Gupta <mukeshgupta.2006@gmail.com>
2010-12-21 21:40:30 +0530

    sl#1504: preserve zoom while rotating
    Earlier image used to go down to its original size while rotating .This patch
    will keep zoom factor into account while rotating
    
    Signed-off-by: Mukesh Gupta <mukeshgupta.2006@gmail.com>

--- ImageView.py
+++ ImageView.py
@@ -197,6 +197,7 @@ class ImageViewer(gtk.DrawingArea):
             logging.warning('Got unsupported rotate angle')
 
         self._temp_pixbuf = self.pixbuf.rotate_simple(rotate)
+        self.set_zoom(self.zoom)
         if self._optimal_zoom_flag == True:
             self.zoom = self.calculate_optimal_zoom(pixbuf=self._temp_pixbuf)
 
